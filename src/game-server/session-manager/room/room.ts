80e8d109529e04c20427f5709d129e6f3f4aad76
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-15 21:32:10 +0200

    feat: update room

---
+++ src/game-server/session-manager/room/room.ts
@@ -0,0 +1,61 @@
+import { WebSocket } from 'ws';
+import { isStr } from '../../../common/utils/index';
+import type { RoomUsers } from '../../common/types';
+import type { Player } from '../session-manager';
+
+export class Room {
+  public static PLAYERS_LIMIT = 2;
+  private _players: Player[] = [];
+
+  constructor(
+    private _id: string,
+    player?: Player,
+  ) {
+    if (player) {
+      this.add(player);
+    }
+  }
+
+  public get players(): Player[] {
+    return [...this._players];
+  }
+
+  public get roomUsers(): RoomUsers {
+    return this._players.map(({ name }) => ({ name, index: name }));
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public add(player: Player): void {
+    if (this._players.length < Room.PLAYERS_LIMIT) {
+      this._players.push(player);
+    }
+  }
+
+  public findPlayer(obj: string | WebSocket | Player): Player | undefined {
+    return this.players.find(p => {
+      if (isStr(obj) && p.name === obj) {
+        return true;
+      }
+      if (obj instanceof WebSocket && p.ws === obj) {
+        return true;
+      }
+      return p === obj;
+    });
+  }
+
+  public has(player?: Player | string): boolean {
+    return Boolean(
+      this._players.find(p => {
+        const name = isStr(player) ? player : player?.name;
+        return p.name === name;
+      }),
+    );
+  }
+
+  public remove(player: Player): void {
+    this._players = this._players.filter(p => p.name !== player.name);
+  }
+}
