d3e6902f0f84366edddb4f3fe44213db8d639ba6
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-16 23:06:46 +0200

    feat: add normal ws server termination

--- src/game-server/ws-server/ws-server.ts
+++ src/game-server/ws-server/ws-server.ts
@@ -6,6 +6,8 @@ import { SessionManager } from '../session-manager/session-manager';
 
 config({ quiet: true });
 
+const NORMAL_WS_CLOSURE = 1001;
+
 const { WS_PORT } = process.env;
 const port = Number(WS_PORT) || DEF_WS_PORT;
 
@@ -14,4 +16,17 @@ void startWebsocketServer({ port }).then(wss => {
   console.log(cyan(`\n🚀 WebSocket server running on ws://localhost:${port}`));
 
   SessionManager.getInstance(wss);
+
+  const cleanUp = (): void => {
+    console.log('\nClosing connections...');
+
+    wss.close(() => {
+      wss.clients.forEach(client => {
+        client.close(NORMAL_WS_CLOSURE);
+      });
+      process.exit(0);
+    });
+  };
+  process.on('SIGTERM', cleanUp);
+  process.on('SIGINT', cleanUp);
 });
